5841d407aec666a7c159355796827542cfee5240
Norbert Borek <norbertborek92@gmail.com>
2022-10-14 18:48:51 +0200

    done

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary < 0) {
+            System.out.println("wrong input!");
+        } else if (salary <= 10000) {
+            double afterTax = (salary - (salary * 0.15));
+            System.out.println(afterTax);
+        } else if (salary <= 20000) {
+            double afterTax = (salary - (salary * 0.18));
+            System.out.println(afterTax);
+        } else {
+            double afterTax = (salary - (salary * 0.20));
+            System.out.println(afterTax);
+        }
+
     }
 }
